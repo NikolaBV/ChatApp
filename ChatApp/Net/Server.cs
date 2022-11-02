7bf304d749f236120fa588e4e6413ec54e9346a7
Nikola Valkov <bulgarianmapper64@gmail.com>
2022-11-02 21:02:51 +0200

    Server class added in Net folder, client interface created

---
+++ ChatApp/Net/Server.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient.Net
+{
+    class Server
+    {
+        TcpClient _client; //
+        public Server() //Instantiete new instance to TCP Client
+        {
+            _client = new TcpClient();
+        }
+        public void ConnectToServer() //Connecting client to server, public because its going to be exposed to the ViewModel
+        {
+            if (!_client.Connected)
+            {
+                _client.Connect("192.168.0.12", 7891);
+            }
+        }
+    }
+}
