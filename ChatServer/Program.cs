5d0116468a08e4644a4cc8fc809d1449122181be
Nikola Valkov <bulgarianmapper64@gmail.com>
2022-11-04 17:09:20 +0200

    Server added, introduced Client class
    Client class servers to create client objects which will be stored in a list in the Program.cs in server project where we also instantiate a TcpListner object

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatServer
+{
+    class Program
+    {
+        static List<Client> _users;
+        static TcpListener _listner;
+        static void Main(string[] args)
+        {
+            _users = new List<Client>();
+            _listner = new TcpListener(IPAddress.Parse("192.168.0.12"), 7891);
+            _listner.Start();
+
+            while (true)
+            {
+                var client = new Client(_listner.AcceptTcpClient());
+                _users.Add(client);
+                /* Broadcast the connection to everyone on the server */
+            }
+
+        }
+    }
+}
